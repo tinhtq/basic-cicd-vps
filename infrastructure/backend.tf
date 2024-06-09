d679fb4c246b63611edc8a95334816130ce3fd9b
Truong Quang Tinh <truongquangtinh1997@gmail.com>
2024-06-09 21:26:41 +0700

    remove unnecessary

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-backend-572551282206"
-    key    = "infrastructure/terraform.tfstate"
-    region = "ap-south-2"
+    bucket = ""
+    key    = ""
+    region = ""
   }
 }
